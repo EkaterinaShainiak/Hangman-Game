40626a7851a3d90989a68b2293b3ca1582b2f615
Ekaterina Shainiak <ekaterina.shainiak@gmail.com>
2017-10-27 00:37:06 -0700

    added html, reset and initialize game function, logic if Char in Word.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,69 @@
-$(document).ready(function() {
+$(document).ready(function () {
     document.addEventListener("keypress", keyPressed)
+    var options = [
+        ["MADONNA", "A PIC", "A SONG"],
+        ["BEATTLES", "A PIC", "A SONG"]
+    ];
+    var word = "";
+    var wins = 0;
+    var hidden_word = "";
+    var inGame = false;
+    var indxCharInHidden = 0;
 
     function keyPressed() {
-        var char = event.key;
-        console.log(char);
+        var char = event.key.toUpperCase();
+        if (isLetter(char) && (!inGame)) {
+            reset();
+            initialize();
+            inGame = true;
+        }
+        indxCharInHidden = word.indexOf(char);
+
+        if (indxCharInHidden != -1) {
+            hidden_word = revealChar(word, hidden_word, char, indxCharInHidden);
+
+            console.log(char);
+            $("#hidden_word").html(hidden_word);
+        }
+        else { console.log("out") };
+
+
+
+
+        function revealChar(word, hidden_word, char, indxCharInHidden) {
+            var tempStr = "";
+            for (i = 0; i < word.length; i++) {
+
+                if (i === indxCharInHidden) {
+                    tempStr += char
+                }
+                else { tempStr += hidden_word[i] }
+
+
+            }
+            return tempStr;
+        };
+
+        function isLetter(char) {
+            return true
+        }
+
+        function reset() {
+            var random_word_inx = Math.floor((Math.random() * 2));
+            word = options[random_word_inx][0];
+            $("#hidden_word").html();
+
+            console.log(word);
+        }
+
+        function initialize() {
+            hidden_word = "";
+            for (i = 0; i < word.length; i++) {
+
+                hidden_word += '-';
+                $("#hidden_word").html(hidden_word);
+            }
+        }
 
     }
-
-
-
-
-
-
-
-
 });
