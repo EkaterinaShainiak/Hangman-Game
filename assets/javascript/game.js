8cea36752b64e649a09d0f9160a374072229552b
Ekaterina Shainiak <ekaterina.shainiak@gmail.com>
2017-10-30 09:40:08 -0700

    removed "empty div"s

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
     var indxCharInHidden = 0;
     var charsCounter = 0;
     var charsRevealed = "";
-    var guesses_remaining = 12;
+    var guesses_remaining = 10;
 
     function keyPressed() {
         var char = event.key.toUpperCase();
@@ -20,23 +20,38 @@ $(document).ready(function () {
             initialize();
             inGame = true;
         }
+        // ifWin();
 
         if (charsRevealed.indexOf(char) == -1) {
             charsRevealed += char
             guesses_remaining -= 1;
+
+
         };
         $("#guessed").text(charsRevealed);
         $("#guesses_num").text(guesses_remaining);
-         
+
         indxCharInHidden = word.indexOf(char);
         if (indxCharInHidden != -1) {
             hidden_word = revealChar(word, hidden_word, char);
             $("#hidden_word").html(hidden_word);
-            
+
         }
 
+        function ifWin() {
+            if (charsRevealed.length == word.length) {
+                console.log("TEST");
+                console.log(charsRevealed.length);
+                console.log(word.length);
+                wins += 1;
+                $("#Wins:").text(wins);
+                reset();
+                initialize();
+            };
 
 
+
+        }
 
         function revealChar(word, hidden_word, char) {
             var tempStr = "";
@@ -56,16 +71,11 @@ $(document).ready(function () {
                 // else { tempStr += hidden_word[i] }
 
                 if (char === word[i]) {
-                    console.log(char);
-                    console.log(word[i])
                     charsCounter += 1;
                     tempStr += char;
 
                 }
                 else { tempStr += hidden_word[i] };
-
-
-
 
             };
             return tempStr;
@@ -78,8 +88,8 @@ $(document).ready(function () {
             var random_word_inx = Math.floor((Math.random() * 2));
             word = options[random_word_inx][0];
             $("#hidden_word").html();
+            ifWin();
 
-            console.log(word);
         }
 
         function initialize() {
@@ -89,6 +99,7 @@ $(document).ready(function () {
                 hidden_word += '-';
                 $("#hidden_word").html(hidden_word);
             }
+            $("#wins").text(wins);
         }
     }
 });
